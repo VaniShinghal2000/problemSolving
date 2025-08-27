a2842369082ae114003b62d460e28ec3bab7736c
VaniShinghal2000 <vanishinghal2000@gmail.com>
2025-08-28 00:29:44 +0530

    Add files via upload
    Initial commit

---
+++ DailyLearnings/src/BasicOne/BasicOne.java
@@ -0,0 +1,93 @@
+package BasicOne;
+
+public class BasicOne {
+
+    //Reverse a string without using built-in functions.
+    public static void ReverseString(){
+        String providedString="vani";
+        String reverseString="";
+        for(int i=providedString.length()-1;i>=0;i--){
+            reverseString=reverseString+providedString.charAt(i);
+
+
+        }
+        System.out.println(reverseString);
+
+
+
+    }
+
+    //Check if a string is a palindrome. //madam
+    public static void IsPalindromeOrNot(){
+        String toCheck="madam";
+        String Empty="";
+        for(int i=toCheck.length()-1;i>=0;i--) {
+            Empty = Empty + toCheck.charAt(i);
+        }
+
+        if(toCheck.equals(Empty)){
+            System.out.println("String is Palindrome");
+        }
+        else{
+            System.out.println("String is not Palindrome");
+        }
+    }
+
+
+    //Swap two numbers without using a third variable.
+    public static void SwapTwoNumbers(){
+
+        int firstNumber=123;
+        int secondNumber=245;
+        System.out.println("Before Swapping numbers are : ");
+        System.out.println("First Number : " +firstNumber + " Second number : "+ secondNumber);
+        firstNumber = firstNumber+secondNumber;
+        secondNumber=firstNumber-secondNumber;
+        firstNumber=firstNumber-secondNumber;
+        System.out.println("After Swapping numbers are : ");
+        System.out.println("First Number : " +firstNumber + " Second number : "+ secondNumber);
+
+
+
+    }
+
+
+    //Count the number of vowels and consonants in a string.
+    public static void countVowelsAndConsonants(){
+        String givenString= "I am a good personooo";
+        String withoutSpace=givenString.replace(" ","");
+        System.out.println(withoutSpace);
+        String vowels="aeiouAEIOU";
+        int countVowels=0;
+        int countConsonants=0;
+        for(int i=0;i<=withoutSpace.length()-1;i++){
+           char ch=withoutSpace.charAt(i);
+           if(ch=='a'|| ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'|| ch=='E'||ch=='I'||ch=='O'||ch=='U'){
+               countVowels++;
+           }
+           else {
+               countConsonants++;
+           }
+        }
+        System.out.println("Number of Vowels = "+ countVowels);
+        System.out.println("Number of Consonants = "+ countConsonants);
+
+
+    }
+
+
+    //Remove duplicate characters from a string.
+
+
+
+    public static void main(String[] args) {
+
+        ReverseString();
+        IsPalindromeOrNot();
+        SwapTwoNumbers();
+        countVowelsAndConsonants();
+
+    }
+
+
+}
